1336d05098dc7e210db9559ffc68c5fe12e11fa7
thirteenmd <andreacojocaru@gmail.com>
2023-02-22 12:27:10 +0200

    moved loading logic to reducer

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,12 +9,14 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({children}) => {
   const initialState = {
     users: [],
-    loading: true
+    loading: false
   }
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
   const fetchUsers = async () => {
+    setLoading()
+
     const res = await fetch(`${GITHUB_URL}/users`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`
@@ -29,6 +31,9 @@ export const GithubProvider = ({children}) => {
     })
   }
 
+  const setLoading = () => {
+    dispatch({type: 'SET_LOADING'})
+  }
   return <GithubContext.Provider value={{ 
       users: state.users, 
       loading: state.loading, 
